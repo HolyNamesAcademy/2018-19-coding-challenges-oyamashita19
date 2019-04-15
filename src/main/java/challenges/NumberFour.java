a6381d92782ef24f2e243b842ea749340d5f49fb
oliviayamashita <oyamashita19@holynames-sea.org>
2019-04-15 07:50:32 -0700

    i did test 4

--- src/main/java/challenges/NumberFour.java
+++ src/main/java/challenges/NumberFour.java
@@ -1,4 +1,5 @@
 package challenges;
+import java.util.ArrayList;
 
 public class NumberFour {
 
@@ -13,7 +14,25 @@ public class NumberFour {
      * @return an array with the specified number of left rotations performed
      */
     public int[] leftRotation(int[] input, int rotations) {
-        // Delete the line below and implement the method!
-        throw new UnsupportedOperationException();
+        int [] rotated = new int[input.length];
+        ArrayList <Integer> newInput = new ArrayList<Integer>();
+        for (int i = 0; i<input.length; i++)
+        {
+            newInput.add(input[i]);
+        }
+        int first;
+for (int j= 0; j<rotations; j++)
+{
+         first = newInput.get(0);
+        newInput.remove(0);
+        newInput.add(first);
+
     }
+    for (int i = 0; i<newInput.size(); i++ )
+    {
+        rotated [i] = newInput.get(i);
+    }
+    return rotated;
+        }
 }
+
